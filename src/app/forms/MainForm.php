874f87f436eb98d5fb57517730170191b89fb996
Diman PRO <diman-pro@myprogram.us>
2016-11-28 15:23:31 +0300

    Добавил получение прокси-серверов и использование одного из них

--- src/app/forms/MainForm.php
+++ src/app/forms/MainForm.php
@@ -6,15 +6,81 @@ use Exception;
 use app\modules;
 use php\gui\framework\AbstractForm;
 use php\gui\event\UXMouseEvent; 
+use php\gui\event\UXEvent; 
 
 
 class MainForm extends AbstractForm
 {
+    function get_and_setProxy(){
+        $host="http://proxyprivat.com/freeproxies";
+        $delStart="<table id='freeProxies' class='display' style=\"display: none\">";
+        $delimerStartIP="<td>";
+        $delimerPort="</div>
+                        </div>
+                    </td>
+                    <td>";
+        $delimerPortEnd="</td>";
+        
+        $all=file_get_contents($host);
+        //echo $all;
+        $ind=strpos($all,$delStart);
+        $IPS=array();
+        $Type=array();
+        $Kol=10;
+        while($ind!=false){
+            $Kol--;
+            if ($Kol<0)
+                break;
+            $tmp=strpos($all,$delimerStartIP,$ind);
+            $ind=$tmp+10;
+                //echo $tmp."---\n";
+            if ($tmp==false){
+                $ind=false;
+                continue;
+            }
+            $strdate=substr($all,$tmp);
+            //echo $strdate."\n";
+            $date=substr($strdate,strlen($delimerStartIP),20);
+            $date=substr($date,0, strpos($date, '<'));
+            $IP=$date;
+            $tmp=strpos($all,$delimerPort,$ind);
+            for ($i=0;$i<1;$i++){
+                $tmp=strpos($all,$delimerPort,$tmp+1);
+            }
+            $strdate=substr($all,$tmp);
+            $value=substr($strdate,strlen($delimerPort),30);
+            $value=substr($value,0, strpos($value, '<'));
+            array_push($IPS,$IP);
+            array_push($Type,$value);
+            echo $IP;
+           $ind=$tmp+80+strlen($delimerPort);
+        }
+        print_r($IPS);
+        for ($x=0;$x<10;$x++){
+            $i=rand(0,count($IPS)-1);
+            if (IPS[$i]==''){
+                continue;
+            }
+            if ($Type[$i]=='')
+                continue;
+            $this->httpClient->proxy=$IPS[$i];
+            $this->editAlt->text=$IPS[$i];
+            if ($Type[$i]=='HTTP(S) прокси'){
+                $this->httpClient->proxyType="HTTP";
+            } else {
+                $this->httpClient->proxyType="SOCKS";
+            }
+            return;
+        }
+    }
     /**
      * @event button.click-Left 
      */
     function doButtonClickLeft(UXMouseEvent $event = null)
-    {   
+    { 
+        echo $this->httpClient->execute("http://2ip.ru")->body();
+        //echo 
+        return;  
         $nr="\n\r";
        
         
@@ -109,6 +175,33 @@ class MainForm extends AbstractForm
             //$tmp=file_get_contents()
 
 
+    }
+
+    /**
+     * @event construct 
+     */
+    function doConstruct(UXEvent $event = null)
+    {    
+        $this->get_and_setProxy();
+    }
+
+    /**
+     * @event buttonAlt.click 
+     */
+    function doButtonAltClick(UXMouseEvent $event = null)
+    {    
+        $del='</span> <big id="d_clip_button">';
+        $res=$this->httpClient->execute("http://2ip.ru")->body();
+        echo 'Site is Loaded'."\n";
+        $tmp=strpos($res,$del);
+        echo 'POS = '.$tmp."\n";
+        $strdate=substr($res,$tmp+strlen($del),20);
+        echo "IP-a".$strdate."\n";
+        //$date=substr($strdate,strlen($del),20);
+        $date=substr($strdate,0, strpos($strdate, '<'));
+        echo " --- ".$date;
+        $IP=$date;
+        $this->textArea->text.='2Ip Send, You IP is '.$IP."\n";
     }
 
 }
